f76019b864c9c32ecfe0128090136876062fb5e0
Rafael Borges <rafael.borges@justdigital.com.br>
2019-05-22 15:09:58 -0300

    Config styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
+import styled from "styled-components";
+import GlobalStyle from "./styles/global";
+
+const Title = styled.h1`
+  color: #f00;
+  font-size: 32px;
+`;
 
 function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <Title>Hello world!!</Title>
+    </Fragment>
+  );
 }
 
 export default App;
